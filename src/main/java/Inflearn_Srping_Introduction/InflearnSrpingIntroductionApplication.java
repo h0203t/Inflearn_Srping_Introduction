0b473afbb0d2331b5491ea898b043929b834e5a3
hyukeh <rnjsgurxo5@naver.com>
2023-11-16 22:05:27 +0900

    Project Upload

---
+++ src/main/java/Inflearn_Srping_Introduction/InflearnSrpingIntroductionApplication.java
@@ -0,0 +1,13 @@
+package Inflearn_Srping_Introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSrpingIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSrpingIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Inflearn_Srping_Introduction/InflearnSrpingIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package Inflearn_Srping_Introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSrpingIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
