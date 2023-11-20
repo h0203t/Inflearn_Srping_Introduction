fe37625331925cec485d4a0f2d4c72ef55da08b4
hyukeh <rnjsgurxo12345@gmail.com>
2023-11-20 18:11:40 +0900

    회원 서비스 개발

---
+++ src/main/java/Inflearn_Srping_Introduction/service/MemberService.java
@@ -0,0 +1,40 @@
+package Inflearn_Srping_Introduction.service;
+
+import Inflearn_Srping_Introduction.domain.Member;
+import Inflearn_Srping_Introduction.repository.MemberRepository;
+import Inflearn_Srping_Introduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+    * 회원가입
+    * */
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 X
+        validataDuplicateMember(member); // 중복 회원 검증
+        memberRepository.sava(member);
+        return member.getId();
+    }
+    private void validataDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원 입니다.");
+                });
+    }
+
+    /*
+    * 회원 검색
+    * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
--- src/test/java/Inflearn_Srping_Introduction/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/Inflearn_Srping_Introduction/repository/MemoryMemberRepositoryTest.java
@@ -1,13 +1,20 @@
 package Inflearn_Srping_Introduction.repository;
 
 import Inflearn_Srping_Introduction.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository = new MemoryMemberRepository();
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void sava() {
@@ -34,4 +41,21 @@ public class MemoryMemberRepositoryTest {
 
         assertThat(result).isEqualTo(member1);
     }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("Spring1");
+        repository.sava(member1);
+
+        Member member2 = new Member();
+        member2.setName("Spring2");
+        repository.sava(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+
 }
