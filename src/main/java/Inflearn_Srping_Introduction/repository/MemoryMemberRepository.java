5aa0e7034bd50839e787651d9445178431d17c78
hyukeh <rnjsgurxo12345@gmail.com>
2023-11-21 18:23:17 +0900

    섹션 4
    회원 관리 예제 끝
    컴포넌트 스캔과 자동 의존관계 설정

--- src/main/java/Inflearn_Srping_Introduction/repository/MemoryMemberRepository.java
+++ src/main/java/Inflearn_Srping_Introduction/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package Inflearn_Srping_Introduction.repository;
 
 import Inflearn_Srping_Introduction.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private Map<Long, Member> store = new HashMap<>();
---
+++ src/test/java/Inflearn_Srping_Introduction/service/MemberServiceTest.java
@@ -0,0 +1,66 @@
+package Inflearn_Srping_Introduction.service;
+
+import Inflearn_Srping_Introduction.domain.Member;
+import Inflearn_Srping_Introduction.repository.MemberRepository;
+import Inflearn_Srping_Introduction.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemberRepository memberRepository;
+
+    @BeforeEach
+    private void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
+    @Test
+    void 회원가입() {
+
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("Spring");
+
+        Member member2 = new Member();
+        member2.setName("Spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
